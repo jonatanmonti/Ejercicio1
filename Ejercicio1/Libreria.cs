3c396aa38854e4c9f8636a66412963f31c0b742e
jonatan monti <jonatan.monti@hotmail.com>
2023-05-10 18:41:23 -0300

    Agregamos la Clase ItemVenta, creamos la lista de Productos y la lista de venta en la clase libreria y el metodo Calcular dentro de la misma clase

--- Ejercicio1/Libreria.cs
+++ Ejercicio1/Libreria.cs
@@ -7,5 +7,27 @@ namespace Ejercicio1
 {
     public class Libreria
     {
-    }
+
+		private List<Producto> productos = new List<Producto>();
+
+		public List<Producto> Productos
+		{
+			get { return productos; }
+			set { productos = value; }
+		}
+
+		private List<Venta> ventas = new List<Venta>();
+
+		public List<Venta> Ventas
+		{
+			get { return ventas; }
+			set { ventas = value; }
+		}
+
+		public void Calcular()
+		{
+
+		}
+
+	}
 }
